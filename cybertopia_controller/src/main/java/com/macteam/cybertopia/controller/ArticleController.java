bd01e09d99666b185259762bf95b58aedbceeb47
Richard Yao <2501.owlsome@gmail.com>
2019-07-05 14:17:26 +0800

    build basic framwork and complete article list

--- cybertopia_controller/src/main/java/com/macteam/cybertopia/controller/ArticleController.java
+++ cybertopia_controller/src/main/java/com/macteam/cybertopia/controller/ArticleController.java
@@ -1,12 +1,21 @@
 package com.macteam.cybertopia.controller;
 
+import com.macteam.cybertopia.entity.Article;
+import com.macteam.cybertopia.pojo.ArticleTitle;
+import com.macteam.cybertopia.service.IArticleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("article")
 public class ArticleController {
+
+    @Autowired
+    private IArticleService articleService;
 
     @RequestMapping("/all.do")
     public String articleList(){
@@ -15,15 +24,31 @@ public class ArticleController {
 
     @RequestMapping("/list.do")
     public String articleListPage(Model model, int pageIndex){
-        System.out.println("请求页面"+pageIndex);
-        if (pageIndex < 30) {
-            model.addAttribute("haveNext",true);
-            model.addAttribute("pageIndex", pageIndex);
-            model.addAttribute("nextIndex", pageIndex+1);
-        } else{
-            model.addAttribute("pageIndex", pageIndex);
-            model.addAttribute("haveNext",false);
-        }
+        int default_pack_size = 10;
+        List<ArticleTitle> article_titles = articleService.getArticleListByRange(
+                pageIndex*default_pack_size, default_pack_size);
+        model.addAttribute("article_titles",article_titles);
+        boolean haveNext = article_titles.size() == default_pack_size;
+        model.addAttribute("haveNext",haveNext);
+        model.addAttribute("pageIndex", pageIndex);
         return "articleListItem";
+    }
+
+    @RequestMapping("/detail.do")
+    public String articleDetail(Model model, int articleId){
+        Article article = articleService.getArticleById(articleId);
+        model.addAttribute("article",article);
+        return "articleDetail";
+    }
+
+    @RequestMapping("/write.do")
+    public String articleWrite(){
+        return "articleWriet";
+    }
+
+    @RequestMapping("/post.do")
+    public String articlePost(Article article){
+        articleService.insertArticle(article);
+        return "redirect:/article/all.do";
     }
 }
--- cybertopia_controller/src/main/webapp/WEB-INF/pages/articleDetail.jsp
+++ cybertopia_controller/src/main/webapp/WEB-INF/pages/articleDetail.jsp
@@ -1,4 +1,4 @@
-<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" isELIgnored="false"%>
 <html lang="en" class="no-js">
 
     <%
@@ -25,7 +25,6 @@
     <script type="text/javascript" src="<%=basePath%>/js/main_js/jquery.bxslider.min.js"></script>
     <script type="text/javascript" src="<%=basePath%>/js/main_js/indexJS.js"></script>
 
-    <script src="<%=basePath%>js/main_js/jquery1.11.3.min.js"></script>
     <script src="<%=basePath%>js/main_js/infinite-scroll.pkgd.min.js"></script>
 </head>
 
@@ -93,35 +92,10 @@
 </div>
 <!--top-->
 
-<!--文章列表-->
 
-<div class="article-list">
-    <div class="article-feed">
-    </div>
-    <div class="scroller-status">
-        <div class="loader-ellips infinite-scroll-request">
-            <span class="loader-ellips__dot"></span>
-            <span class="loader-ellips__dot"></span>
-            <span class="loader-ellips__dot"></span>
-            <span class="loader-ellips__dot"></span>
-        </div>
-        <p class="scroller-status__message infinite-scroll-last">End of content</p>
-        <p class="scroller-status__message infinite-scroll-error">No more pages to load</p>
-    </div>
-    <p class="pagination">
-        <a class="pagination__next" href="<%=basePath%>/article/list.do?pageIndex=1">Next page</a>
-    </p>
-    <script>
-        var elem = document.querySelector('.article-feed');
-        var infScroll = new InfiniteScroll( elem, {
-            path: '.pagination__next',
-            append: '.article_titles',
-            status: '.scroller-status',
-            hideNav: '.pagination',
-            prefill: true,
-            history: false,
-            scrollThreshold: 400
-        });
-    </script>
-</div>
+
+
+
 </body>
+</html>
+
